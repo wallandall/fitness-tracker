4615788cb2cb4c25f2194ee4ab1a7a0274ec5603
wallandall <wallandall@gmail.com>
2018-11-25 13:57:15 +0100

    Added FireStore and data table

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAbX7pX2Ptn_nIXea8ZsJQyoYx1OIWhWKQ",
+    authDomain: "webe-fitness-trackers.firebaseapp.com",
+    databaseURL: "https://webe-fitness-trackers.firebaseio.com",
+    projectId: "webe-fitness-trackers",
+    storageBucket: "webe-fitness-trackers.appspot.com",
+    messagingSenderId: "898908915597"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
